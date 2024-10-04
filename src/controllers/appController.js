f623c30b26238b5780ed3f66859ec7ac0de067f8
albertverso <caralbertverso@gmail.com>
2024-10-04 14:31:51 -0300

    fix route app

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -1,5 +1,5 @@
 const verificando = (req, res) => {
-    res.send('FUNCIONANDO MEU QUERIDO!!!');
+    res.status(200).json({message: 'API funcionando corretamente'});
   }
 
 module.exports = verificando
