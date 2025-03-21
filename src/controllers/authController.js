6329c0191c3f44966865b21415c95e23b609369b
Carlos Alberto <caralbertverso@gmail.com>
2025-03-21 16:44:27 -0300

    modificated endpoint login and created endpoint logout

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,7 +11,7 @@ const login = async (req, res) => {
 
       // Se o usuário não for encontrado ou a senha estiver incorreta
       if (!user) {
-         return res.status(401).json({ message: 'Credenciais inválidas' });
+         return res.status(404).json({ message: 'Credenciais inválidas' });
       }
 
       const passwordMatch = await bcrypt.compare(password, user.password);
@@ -27,12 +27,31 @@ const login = async (req, res) => {
          { expiresIn: '1h' } // Token expira em 1 hora (opcional)
       );
 
+      // Atualizar o token no banco de dados do usuário
+      await User.update(
+         { currentToken: token }, // Salva o token gerado
+         { where: { id: user.id } }
+      );
+
       return res.json({ token });
    } catch (error) {
       return res.status(500).json({ message: 'Erro no servidor' });
    }
 };
 
+const logout = async (req, res) => {
+   try {
+      await User.update(
+         { currentToken: null }, // Removemos o token
+         { where: { id: req.user.id } }
+      );
+
+      return res.json({ message: "Logout realizado com sucesso" });
+   } catch (error) {
+      return res.status(500).json({ message: "Erro ao fazer logout" });
+   }
+};
+
 module.exports = {
-   login
+   login, logout
 };
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,5 +1,9 @@
 const app = require('../routes/appRoute.js');
 const {login} = require('../controllers/authController.js')
+const {logout} = require('../controllers/authController.js')
 
 // Endpoint de login
 app.post('/v1/login', login);
+
+// Endpoint de logout
+app.post('/v1/logout', logout);
